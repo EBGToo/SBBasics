4892f854d7ab457473d4b9e52f30eb0581c42525
Ed Gamble <ebg@opuslogica.com>
2016-07-17 16:39:51 -0700

    Remove dependency on 'Foundation'

--- Sources/SBBasics.h
+++ Sources/SBBasics.h
@@ -5,15 +5,3 @@
 //  Created by Ed Gamble on 11/20/15.
 //  Copyright © 2015 Opus Logica Inc. All rights reserved.
 //
-
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for SBBasics.
-FOUNDATION_EXPORT double SBBasicsVersionNumber;
-
-//! Project version string for SBBasics.
-FOUNDATION_EXPORT const unsigned char SBBasicsVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SBBasics/PublicHeader.h>
-
-
